7640f2ccf07a910a77b431cea0cdede58b30be67
unkascrack <carlos.alonso.gonzalez@gmail.com>
2015-09-09 00:47:57 +0200

    added custom datasource to manage transactions

---
+++ src/main/java/com/github/lucene/store/database/ConnectionProxy.java
@@ -0,0 +1,21 @@
+package com.github.lucene.store.database;
+
+import java.sql.Connection;
+
+/**
+ * Subinterface of Connection to be implemented by connection proxies.
+ * <p/>
+ * Initial version taken from Spring.
+ *
+ * @author kimchy
+ */
+public interface ConnectionProxy extends Connection {
+
+    /**
+     * If the given Jdbc Connection actually controls the connection.
+     *
+     * @see TransactionAwareDataSourceProxy
+     * @see DataSourceUtils#releaseConnection(java.sql.Connection)
+     */
+    boolean isControlConnection();
+}
